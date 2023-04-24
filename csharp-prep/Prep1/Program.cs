85b2d583039379b4a2b65d48b48442971101cf88
zchexmix <zachchecketts9@gmail.com>
2023-04-24 15:14:21 +0100

    James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {last_name}, {name} {last_name}.");
     }
 }
