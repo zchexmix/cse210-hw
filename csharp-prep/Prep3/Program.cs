a850bc0d4db5b47b62f27edf3fb709bf42551d09
zchexmix <zachchecketts9@gmail.com>
2023-05-04 15:43:11 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Greater");
+            }
+
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("Thats Correct!");
+            }
+        }
     }
 }
