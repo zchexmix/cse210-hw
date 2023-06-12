54500a07a76461468668ae7e5a4d33125ff218b3
zchexmix <zachchecketts9@gmail.com>
2023-06-12 21:11:32 +0100

    Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Console.WriteLine(first.GetFraction());
+        Console.WriteLine(first.GetDecimal());
+
+
+        Fraction second = new Fraction(5);
+        Console.WriteLine(second.GetFraction());
+        Console.WriteLine(second.GetDecimal());
+
+
+        Fraction third = new Fraction(3, 4);
+        Console.WriteLine(third.GetFraction());
+        Console.WriteLine(third.GetDecimal());
+
+        Fraction fourth = new Fraction(1, 3);
+        Console.WriteLine(fourth.GetFraction());
+        Console.WriteLine(fourth.GetDecimal());
+
+
     }
 }
